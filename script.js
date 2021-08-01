ec743f8626f9ed3d2714e9f1395b56119508e608
shuvo18103107 <18103107@iubat.edu>
2021-08-01 12:49:45 +0600

    📝  project template setup

--- script.js
+++ script.js
@@ -3,29 +3,30 @@
 // BANKIST APP -mimimalist online banking
 
 // Data
+//using object rather than map cg data are coming from web api are like object
 const account1 = {
-    owner: 'Jonas Schmedtmann',
+    owner: 'Mohammad Ali Shuvo',
     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
     interestRate: 1.2, // %
     pin: 1111,
 };
 
 const account2 = {
-    owner: 'Jessica Davis',
+    owner: 'Noushad Bhuiyan',
     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
     interestRate: 1.5,
     pin: 2222,
 };
 
 const account3 = {
-    owner: 'Steven Thomas Williams',
+    owner: 'A.K.M Miftahur Rahman Sarker',
     movements: [200, -200, 340, -300, -20, 50, 400, -460],
     interestRate: 0.7,
     pin: 3333,
 };
 
 const account4 = {
-    owner: 'Sarah Smith',
+    owner: 'Abdullah',
     movements: [430, 1000, 700, 50, 90],
     interestRate: 1,
     pin: 4444,
@@ -57,7 +58,7 @@ const inputTransferTo = document.querySelector('.form__input--to');
 const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+const inputClosePin = document.querySelector('.form__input-- ');
 
 
 
