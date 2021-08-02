62f385bc2f29368ffad00224eab7364593f91078
shuvo18103107 <18103107@iubat.edu>
2021-08-02 22:20:40 +0600

    ✅ understanding map in deep

---
+++ dataTransformation.js
@@ -0,0 +1,32 @@
+'use strict';
+// Data transformation with map , filter , and reduce
+
+ //this are array method use to create new array based on transfer data 
+
+/*
+1. Map
+Map is another way to loop over an array like forEach method but map create a new array based on orginal array
+
+[3,1,4,3,2]
+apply map -> current*2
+return new array
+[6 2 8 6 4]
+forEach just array er upor iterate kore kaj korte parto but map iterate er pasapasi brand new ekta array return kore
+
+2. Filter
+
+[3 1 4 3 2]
+using filter (current>2)
+[3 4 3]
+
+3. Reduce Method
+adding all element together
+
+[3 1 4 3 2]
+its like snow ball getting bigger and bigger
+  accumulator +current
+
+  13
+this method only return value not new array
+
+*/
