64dab27c52efc550f3aab6232d39af9c4ffc4433
shuvo18103107 <18103107@iubat.edu>
2021-08-06 00:30:25 +0600

    ✅ converting and checking numbers

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ const account1 = {
     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
     interestRate: 1.2, // %
     pin: 1111,
+    movementsDates: [
+        '2019-11-18T21:31:17.178Z',
+        '2019-12-23T07:42:02.383Z',
+        '2020-01-28T09:15:04.904Z',
+        '2020-04-01T10:17:24.185Z',
+        '2020-05-08T14:11:59.604Z',
+        '2020-07-26T17:01:17.194Z',
+        '2020-07-28T23:36:17.929Z',
+        '2020-08-01T10:51:36.790Z',
+    ],
+    currency: 'EUR',
+    locale: 'pt-PT', // de-DE
+
 };
 
 const account2 = {
@@ -16,23 +29,35 @@ const account2 = {
     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
     interestRate: 1.5,
     pin: 2222,
+    movementsDates: [
+        '2019-11-01T13:15:33.035Z',
+        '2019-11-30T09:48:16.867Z',
+        '2019-12-25T06:04:23.907Z',
+        '2020-01-25T14:18:46.235Z',
+        '2020-02-05T16:33:06.386Z',
+        '2020-04-10T14:43:26.374Z',
+        '2020-06-25T18:49:59.371Z',
+        '2020-07-26T12:01:20.894Z',
+    ],
+    currency: 'USD',
+    locale: 'en-US',
 };
 
-const account3 = {
-    owner: 'A.K.M Miftahur Rahman Sarker',
-    movements: [200, -200, 340, -300, -20, 50, 400, -460],
-    interestRate: 0.7,
-    pin: 3333,
-};
+// const account3 = {
+//     owner: 'A.K.M Miftahur Rahman Sarker',
+//     movements: [200, -200, 340, -300, -20, 50, 400, -460],
+//     interestRate: 0.7,
+//     pin: 3333,
+// };
 
-const account4 = {
-    owner: 'John Doe',
-    movements: [430, 1000, 700, 50, 90],
-    interestRate: 1,
-    pin: 4444,
-};
+// const account4 = {
+//     owner: 'John Doe',
+//     movements: [430, 1000, 700, 50, 90],
+//     interestRate: 1,
+//     pin: 4444,
+// };
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -344,8 +369,10 @@ btnClose.addEventListener('click', function (e) {
         //indexof o kora jai but indexof findindex er moto complex query kora jai na just array te data thakle kaj kore like indexof(23)
         accounts.splice(clsAccUser, 1);
 
+
         // hide UI
         containerApp.style.opacity = 0;
+        labelWelcome.textContent = 'Log in to get started';
         console.log(`Account close successfull`);
 
         console.log(accounts);
@@ -527,3 +554,34 @@ labelBalance.addEventListener('click', function () {
     console.log(moveUi2);
     console.log(movemntUI);
 });
+
+console.log('------------------------Numbers and Date--------------------');
+//-----------Numbers-----------
+//js always shows decimal point in any numbers
+console.log(23 === 23.0);
+
+console.log(0.1 + 0.2 === 0.3);
+//but js return false in that case due to fractional system
+// string to number
+console.log(Number('23'));
+console.log(+'23');//+ here typr conversion convert string to num autmatically
+
+//Parsint - identify number from a string- but string should be start with number otherwise not work
+//2nd parameter is base base10- 0-9 - sometimes it help to unencessary bug
+console.log(Number.parseInt('100px', 10));
+console.log(Number.parseInt('px100', 10));
+console.log(Number.parseFloat('2.5rem', 10));
+console.log(Number.parseInt('2.5rem', 10));
+
+// we can check if any value is a number or not a number (NAN hole true noile false)
+
+console.log(Number.isNaN('shuvo'));
+console.log(Number.isNaN(+'20x'));
+// use isfinite rather than isnan to check a value is number or nan
+//best way to checking a value is number or not 
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20x'));
+console.log(Number.isFinite(23 / 0));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23.0 / 0));
